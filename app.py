3c42d43f88a215db3391a33e5da6b3aad59232ee
Taran <taran@Tarans-MacBook-Air.local>
2023-11-18 13:43:40 +0530

    update

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
-import matplotlib
-matplotlib.use('Agg')  # Set the Matplotlib backend to 'Agg'
-import matplotlib.pyplot as plt
-import yfinance as yf
+import os
 import pandas as pd
+import yfinance as yf
+import matplotlib
+matplotlib.use('Agg')  # Use non-interactive backend
+import matplotlib.pyplot as plt
 from datetime import datetime
 import base64
 from io import BytesIO
+
+# Set MPLCONFIGDIR to a writable directory
+os.environ['MPLCONFIGDIR'] = '/tmp'
 
 app = Flask(__name__)
 
 csv_file = 'stock_purchases.csv'
 
+# Function to read stock purchases from CSV
 def read_stock_purchases():
     try:
         return pd.read_csv(csv_file)
     except FileNotFoundError:
         return pd.DataFrame(columns=['Ticker', 'BuyDate', 'BuyPrice', 'Quantity'])
 
+# Function to write stock purchases to CSV
 def write_stock_purchases(df):
     df.to_csv(csv_file, index=False)
 
+# Function to fetch stock data
 def fetch_stock_data(stock_purchases):
     stock_data = {}
     end_date = datetime.now()
@@ -30,6 +37,7 @@ def fetch_stock_data(stock_purchases):
         buy_date = pd.to_datetime(row['BuyDate'])
         buy_price = float(row['BuyPrice'])
         quantity = int(row['Quantity'])
+
         try:
             tickerData = yf.Ticker(ticker)
             data = tickerData.history(start=buy_date, end=end_date)
@@ -37,6 +45,7 @@ def fetch_stock_data(stock_purchases):
             percentage_change = ((current_price - buy_price) / buy_price) * 100
             performance = "Up" if current_price > buy_price else "Down"
 
+            # Generate graph
             plt.figure(figsize=(10, 4))
             plt.plot(data['Close'])
             plt.title(f"{ticker} Stock Price")
@@ -56,8 +65,10 @@ def fetch_stock_data(stock_purchases):
             }
         except Exception as e:
             print(f"Error fetching data for {ticker}: {e}")
+
     return stock_data
 
+# Function to get graph image
 def get_graph():
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
@@ -68,6 +79,7 @@ def get_graph():
     buffer.close()
     return graph
 
+# Route for the main page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -86,7 +98,6 @@ def index():
     stock_data = fetch_stock_data(read_stock_purchases())
     return render_template('stocks.html', stock_data=stock_data)
 
-# The below line is not needed for Vercel deployment
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 

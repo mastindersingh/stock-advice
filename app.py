55a125cebd66cd08e98d6f9b1b0bb45e8bd8d44c
Taran <taran@Tarans-MacBook-Air.local>
2023-11-20 00:22:33 +0530

    update

--- app.py
+++ app.py
@@ -7,17 +7,24 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime
 import base64
+import os
 from io import BytesIO
 
 app = Flask(__name__)
 app.secret_key = 'bApG1HXBfOeC5JhRj_tvKA'  # Replace with your real secret key
 
+# Example hardcoded credentials
+users = {
+    'user1@yahoo.com': 'password1',
+    'user2': 'password2'
+}
+
 # Google OAuth Setup
 oauth = OAuth(app)
 google = oauth.remote_app(
     'google',
-    consumer_key='72321166098-rqs54h296h3pp6clb1h19cn7bp4rp8rn.apps.googleusercontent.com',  # Replace with your Google Client ID
-    consumer_secret='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',  # Replace with your Google Client Secret
+    consumer_key='72321166098-rqs54h296h3pp6clb1h19cn7bp4rp8rn.apps.googleusercontent.com',  # Your Google Client ID
+    consumer_secret='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',  # Your Google Client Secret
     request_token_params={'scope': 'email'},
     base_url='https://www.googleapis.com/oauth2/v1/',
     request_token_url=None,
@@ -29,8 +36,16 @@ google = oauth.remote_app(
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Manual login logic (if you have one)
-        pass
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username in users and users[username] == password:
+            session['logged_in'] = True
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error="Invalid credentials.")
+
     return render_template('login.html')
 
 @app.route('/google-login')
@@ -42,6 +57,8 @@ def logout():
     session.pop('google_token', None)
     session.pop('email', None)
     session.pop('subscribed', None)
+    session.pop('logged_in', None)
+    session.pop('username', None)
     return redirect(url_for('login'))
 
 @app.route('/login/authorized')
@@ -74,18 +91,26 @@ def subscribe():
 
 def check_subscription_code(code):
     try:
-        codes_df = pd.read_csv('subscription_codes.csv')
-        return code in codes_df['code'].values
-    except FileNotFoundError:
-        return False
+        file_path = 'subscription_codes.csv'
+        print(f"Reading file from: {os.path.abspath(file_path)}")  # Print the absolute file path
 
+        codes_df = pd.read_csv(file_path)
+        print("Contents of the file:")  # Debug: Print the DataFrame to check its contents
+        print(codes_df)
+
+        # Ensure the code is checked as a string
+        code = str(code)
+        return code in codes_df['code'].astype(str).values
+    except FileNotFoundError:
+        print("File not found.")
+        return False
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'email' not in session:
+    if 'logged_in' not in session and 'email' not in session:
         return redirect(url_for('login'))
 
     # Check if the user is subscribed
-    if not session.get('subscribed', True):
+    if not session.get('subscribed', False):
         return redirect(url_for('subscribe'))
 
     # If the user is subscribed, display the stock data
@@ -151,6 +176,12 @@ def get_graph():
     buffer.close()
     return graph
 
+def test_check_subscription_code():
+    test_code = "12345"  # Replace with a test code
+    result = check_subscription_code(test_code)
+    print(f"Test Code: {test_code}, Result: {result}")
+
 if __name__ == '__main__':
+    test_check_subscription_code()  # Test function call
     app.run(debug=True)
 
